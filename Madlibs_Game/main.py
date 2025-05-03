5dccd51ff492a8453312d5bf39947c992810afea
kartikgupta666 <kartikgangil@gmail.com>
2025-05-04 01:18:33 +0530

    first Madlibs_Game commit

---
+++ Madlibs_Game/main.py
@@ -0,0 +1,18 @@
+# this is the game in which , we use random words and make a story
+
+
+noune1 = input("enter the name of the person , place or thing : ")
+noune2 = input("enter the name of the person, place or thing : ")
+emotion = input("enter the emotion : ")
+game_name = input("enter the name of the sport : ")
+place = input("enter the name of the place : ")
+food = input("enter the food name : ")
+drink = input("enter the drink name : ")
+
+print("Welcome to madlibs game")
+print(f"Today , {noune1} going to park, ")
+print(f"{noune1} was very {emotion} to hit the daily routiene ")
+print(f"Then , {noune1} saw a {noune2} , {noune2} was playing {game_name}")
+print(f"{noune1} also play with {noune2} , and we had a lot of fun")
+print(f"Then {noune1} and {noune2} went to {place} and had a {food} and {drink}")
+print(f"this is the story of the {noune1} nad {noune2}")
